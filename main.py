93afb5e52470c365f85b4aeaedec2bbb33edbdc0
TomMuet <tom.muet@mines-ales.org>
2023-07-10 12:54:16 +0200

    delete

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 from functools import wraps
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
-# At the top of your script, you import logging
 import logging
 
-# Setup your logging
 logger = logging.getLogger('family_logger')
 logger.setLevel(logging.INFO)
-
-# This prevents Python from setting up the default logging configuration
 logging.getLogger().handlers = []
-
-# Here we define a Handler which writes INFO messages or higher to a file
 file_handler = logging.FileHandler('family_additions.log')
 file_handler.setLevel(logging.INFO)
-
-# We also need a formatter to specify what our log lines will look like
 formatter = logging.Formatter('%(asctime)s - %(message)s')
 file_handler.setFormatter(formatter)
-
-# Here we add our handler to the logger
 logger.addHandler(file_handler)
 
 app = Flask(__name__)
@@ -28,7 +18,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///familles.db'
 db = SQLAlchemy(app)
 
 logins = {"comitedesfetes": "vieillevigne"}
-
 
 class Famille(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -40,14 +29,11 @@ class Famille(db.Model):
     def __repr__(self):
         return f'<Famille {self.nom}>'
 
-
 def create_database():
     with app.app_context():
         db.create_all()
 
-
 create_database()
-
 
 def login_required(f):
     @wraps(f)
@@ -55,9 +41,7 @@ def login_required(f):
         if "logged_in" not in session:
             return redirect(url_for("login", next=request.url))
         return f(*args, **kwargs)
-
     return decorated_function
-
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
@@ -70,15 +54,12 @@ def login():
             return redirect(url_for("index"))
         else:
             error = "Identifiants incorrects"
-
     return render_template("login.html", error=error)
-
 
 @app.route("/logout")
 def logout():
     session.pop("logged_in", None)
     return redirect(url_for("login"))
-
 
 @app.route("/", methods=["GET", "POST"])
 @login_required
@@ -92,39 +73,38 @@ def index():
         nb_personnes = int(request.form.get("nb_personnes"))
         enfants_moins_12_ans = request.form.get("enfants_moins_12_ans")
         enfants_moins_12_ans = int(enfants_moins_12_ans) if enfants_moins_12_ans != "" else None
-        prix = (
-                           nb_personnes - enfants_moins_12_ans) * normal_price + enfants_moins_12_ans * child_price if enfants_moins_12_ans is not None else nb_personnes * normal_price
-        famille = Famille(nom=nom_famille, nb_personnes=nb_personnes, enfants_moins_12_ans=enfants_moins_12_ans,
-                          prix=prix)
+        prix = (nb_personnes - enfants_moins_12_ans) * normal_price + enfants_moins_12_ans * child_price if enfants_moins_12_ans is not None else nb_personnes * normal_price
+        famille = Famille(nom=nom_famille, nb_personnes=nb_personnes, enfants_moins_12_ans=enfants_moins_12_ans, prix=prix)
         db.session.add(famille)
         db.session.commit()
-        logger.info(
-            f'Family added: {famille.nom}, {famille.nb_personnes} people, {famille.enfants_moins_12_ans if famille.enfants_moins_12_ans is not None else "No"} children under 12')
+        logger.info(f'Family added: {famille.nom}, {famille.nb_personnes} people, {famille.enfants_moins_12_ans if famille.enfants_moins_12_ans is not None else "No"} children under 12')
 
     familles = Famille.query.all()
     total_personnes = sum([f.nb_personnes for f in familles])
 
     for famille in familles:
         if famille.enfants_moins_12_ans:
-            total_revenue += (
-                                         famille.nb_personnes - famille.enfants_moins_12_ans) * normal_price + famille.enfants_moins_12_ans * child_price
+            total_revenue += (famille.nb_personnes - famille.enfants_moins_12_ans) * normal_price + famille.enfants_moins_12_ans * child_price
         else:
             total_revenue += famille.nb_personnes * normal_price
 
-    return render_template("index.html", familles=familles, total_personnes=total_personnes,
-                           total_revenue=total_revenue, prix=normal_price, prix_enfant=child_price)
-
+    return render_template("index.html", familles=familles, total_personnes=total_personnes, total_revenue=total_revenue, prix=normal_price, prix_enfant=child_price)
 
 @app.route("/reset", methods=["POST"])
 @login_required
 def reset():
-    # Supprimer toutes les familles de la base de données
     db.session.query(Famille).delete()
     db.session.commit()
-
-    # Rediriger vers la page d'accueil
     return redirect(url_for("index"))
 
+@app.route("/delete/<id>", methods=["POST"])
+@login_required
+def delete(id):
+    famille = Famille.query.get(id)
+    if famille:
+        db.session.delete(famille)
+        db.session.commit()
+    return redirect(url_for("index"))
 
 @app.route("/imprimer")
 @login_required
@@ -132,9 +112,7 @@ def imprimer():
     familles = Famille.query.all()
     total_personnes = sum([famille.nb_personnes for famille in familles])
     total_revenue = sum([famille.prix for famille in familles])
-    return render_template("imprimer.html", familles=familles, total_personnes=total_personnes,
-                           total_revenue=total_revenue)
-
+    return render_template("imprimer.html", familles=familles, total_personnes=total_personnes, total_revenue=total_revenue)
 
 if __name__ == "__main__":
     with app.app_context():
